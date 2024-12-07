de20c79c0e6ccac641dccdfc7f69cfa4d48c79c0
william25885 <william25885@gmail.com>
2024-12-08 02:01:14 +0800

    Update login.py

--- actions/auth/login.py
+++ actions/auth/login.py
@@ -12,41 +12,15 @@ login = Blueprint("login", __name__)
 class LoginAction(Action):
     def __init__(self, db_manager):
         self.db_manager = db_manager
-        self.cur = db_manager
         
     def exec(self):
         # 讀取帳號密碼
         account = request.form.get('account')  # 從 POST 請求中獲取帳號
         password = request.form.get('password')  # 從 POST 請求中獲取密碼
         
-        cmd = """
-            select u.account, u.password
-            from user as u
-            where u.account = %s and u.password = %s
-            """
-        
-        # 執行查詢
-        self.cur.execute(cmd, [account, password])
-        users = self.cur.fetchall()
+        self.db_manager.verify_login(account, password)
 
-        if len(users) != 1:
-            # 登入失敗
-            response = {
-                "status": "error",
-                "message": "Invalid account or password!"
-            }
-            return jsonify(response)
-        else:
-            # 根據角色生成回應
-            response = {
-                "status": "success",
-                "message": f"Welcome back, {users[0]['user_name']}!",
-                "user_id": users[0]["user_id"],
-                "role": users[0]["role"]
-            }
-            return jsonify(response)
-
-login_action = LoginAction(db_manager = None)
+login_action = LoginAction(db_manager)
 
 @login.route('/submit-login', methods=['POST'])
 def login_route():
