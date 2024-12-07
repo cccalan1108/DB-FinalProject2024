98558b8554d79dce0b412f85738bb6404f47e540
william25885 <william25885@gmail.com>
2024-12-08 07:18:20 +0800

    Update app.py

--- actions/app.py
+++ actions/app.py
@@ -5,18 +5,24 @@ from auth.signup import signUp
 from flask_cors import CORS
 
 # Global Flask app (SUBJECT TO CHANGE) static_folder="../frontend/assets,template_folder="../frontend/html""
-app = Flask(__name__, template_folder="../frontend/html", static_folder="../frontend/css")
+app = Flask(__name__, static_folder="../frontend/css", template_folder="../frontend/html")
 CORS(app, resources={r"*": {"origins": "*"}}) 
 
-script_bp = Blueprint('script_bp', __name__, static_folder='../frontend/script')
+script_bp = Blueprint('script', __name__, static_folder='../frontend/script', static_url_path='/script')
 app.register_blueprint(script_bp)
 app.register_blueprint(login)
 app.register_blueprint(signUp)
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
 
 
 @app.route('/')
 def index():
     return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug = True)
