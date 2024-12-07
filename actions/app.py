5eed4ceb278f2e72644d6442f253b7988c8d3217
william25885 <william25885@gmail.com>
2024-12-08 03:04:59 +0800

    Update app.py

--- actions/app.py
+++ actions/app.py
@@ -2,16 +2,14 @@ from flask import Flask, render_template, session, redirect
 from DB_utils import db_connect
 
 from actions.auth.login import login
-
+from actions.auth.signup import signUp
 
 # Global Flask app (SUBJECT TO CHANGE) static_folder="../frontend/assets"
 app = Flask(__name__, template_folder="../frontend/html")
 
 app.register_blueprint(login)
+app.register_blueprint(signUp)
 
-def init_app():
-    db_connect()
-    
 @app.route('/')
 def index():
     if session.get("login"):
