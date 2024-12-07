f6f3f5bab87d48a2a61ffe0efec34df621ca526a
cccalan1108 <cccalan1108@gmail.com>
2024-12-08 07:30:31 +0800

    n

--- actions/app.py
+++ actions/app.py
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 from flask import Flask, render_template, session, redirect, Blueprint 
-=======
-from flask import Flask, render_template, session, redirect, Blueprint
->>>>>>> 3920b0020d0640573ceec45ad3072086ca1973c2
 
 from auth.login import login
 from auth.signup import signUp
 from meeting.create_meeting import create_meeting
 from meeting.list_meeting import list_meeting
-#from flask_cors import CORS
+from flask_cors import CORS
 
 # Global Flask app (SUBJECT TO CHANGE) static_folder="../frontend/assets,template_folder="../frontend/html""
 app = Flask(__name__, static_folder="../frontend/css", template_folder="../frontend/html")
@@ -24,7 +20,6 @@ app.register_blueprint(list_meeting)
 @app.route('/register')
 def register():
     return render_template('register.html')
-
 
 
 @app.route('/')
