8d4da21e52e10b55ab8b2b5aac17ed5a107f8cd8
william25885 <william25885@gmail.com>
2024-12-08 06:22:14 +0800

    Update login.js

--- frontend/script/login.js
+++ frontend/script/login.js
@@ -1,11 +1,13 @@
 // 替form添加監聽器聽submit
 document.getElementById('login-form').addEventListener('submit', async (e) => {
     // 防止表單提交刷新頁面而重整
+    console.log("DEBUG: Event listener triggered");
     e.preventDefault();
 
     const account = document.getElementById('account').value;
     const password = document.getElementById('password').value;
-    
+    console.log(account)
+    console.log(password)
     try{
         const response = await fetch('/submit-login', {
             method: 'POST',
@@ -15,6 +17,12 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
             body: new URLSearchParams({ account, password }),
         });
 
+        if (!response.ok) {
+            const errorData = await response.json();
+            console.log("noook")
+            throw new Error(errorData.message || 'Unexpected error occurred');
+        }
+        console.log("ok")
         // 解析 JSON
         const data = await response.json();
         if (data.status === 'success') {
