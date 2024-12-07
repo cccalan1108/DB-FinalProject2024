a5e646d4178944ed2b4a3a61283357f9aff5af9f
william25885 <william25885@gmail.com>
2024-12-08 00:35:43 +0800

    Create app.py

---
+++ actions/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, session, redirect
+from DB_utils import db_connect
+
+from actions.auth.login import login
+
+
+# Global Flask app (SUBJECT TO CHANGE) static_folder="../frontend/assets"
+app = Flask(__name__, template_folder="../frontend/html")
+
+app.register_blueprint(login)
+
+def init_app():
+    db_connect()
+    
+@app.route('/')
+def index():
+    if session.get("login"):
+        return redirect("/category")
+    else:
+        return redirect("/login") 
+
+if __name__ == '__main__':
+    init_app()
+    app.run()
